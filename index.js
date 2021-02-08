474658c6e3b55ea818b26a043a1c848305daa292
Kevin Parra <53342165+Kevinparra535@users.noreply.github.com>
2021-02-08 13:10:58 -0500

    [AÑADIDO] Contenido de las tabs

--- index.js
+++ index.js
@@ -24,23 +24,3 @@ function openTab(e, dataID) {
 }
 
 document.getElementById("defaultOpen").click();
-
-// function openTab(e, cityName) {
-//   e.preventDefault();
-
-//   var i, tabcontent, tablinks;
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-
-//   tablinks = document.getElementsByClassName("tablinks");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
-
-//   document.getElementById(cityName).style.display = "block";
-//   e.currentTarget.className += " active";
-// }
-
-// document.getElementById("defaultOpen").click();
